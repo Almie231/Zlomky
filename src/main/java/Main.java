ffa9eef778ef8f466faee1fe663b276265872a2b
sterbto1 <sterbto1@uhk.cz>
2026-02-09 13:05:58 +0100

    1. Cviceni -> 3. zmena (Pridany akce a trida Zlomek)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import cz.uhk.zlomky.Zlomek;
 
 public class Main {
     static void main() {
         IO.println("Projekt Zlomky s novou JDK 25");
         IO.println("=============================");
+        var a = new Zlomek();
+        a.setCitatel(3);
+        a.setJmenovatel(4);
+        IO.println("Zlomek : "+a.getCitatel()+"/"+a.getJmenovatel());
+        Zlomek b = new Zlomek(4,5);
+        IO.println(b);
     }
 }
---
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -0,0 +1,35 @@
+package cz.uhk.zlomky;
+
+public class Zlomek {
+    private int citatel;
+    private int jmenovatel;
+
+    public Zlomek() {}
+
+    public Zlomek(int citatel, int jmenovatel) {
+    this.citatel = citatel;
+    this.jmenovatel = jmenovatel;
+    }
+
+
+    //getter
+    public int getCitatel() {
+        return citatel;
+    }
+    public int getJmenovatel() {
+        return jmenovatel;
+    }
+
+    //setter
+    public void setCitatel(int citatel){
+        this.citatel = citatel;
+    }
+    public void setJmenovatel(int jmenovatel){
+        this.jmenovatel = jmenovatel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%4d\n-------\n%4d",citatel,jmenovatel);
+    }
+}
