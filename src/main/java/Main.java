df8d4eaf7aa814fca15449b26521678c3682ea50
sterbto1 <sterbto1@uhk.cz>
2026-02-16 12:49:39 +0100

    2. Cviceni -> 2. zmena (Pridany metody plus, minus, krat, deleno a privatni metoda zkratit, zaroven bylo pridano pole zlomku a pocitani jeho prumeru, odstraneny 1. testy z 1. cviceni)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,36 @@ import cz.uhk.zlomky.Zlomek;
 
 public class Main {
     static void main() {
-        IO.println("Projekt Zlomky s novou JDK 25");
-        IO.println("=============================");
-        var a = new Zlomek();
-        a.setCitatel(3);
-        a.setJmenovatel(4);
-        IO.println("Zlomek : "+a.getCitatel()+"/"+a.getJmenovatel());
-        Zlomek b = new Zlomek(4,5);
-        IO.println(b);
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(1, 2);
+
+        var soucet = a.plus(b);
+        IO.println("Soucet je %s".formatted(soucet.zkratit()));
+        var rozdil = a.minus(b);
+        IO.println("Rozdil je %s".formatted(rozdil.zkratit()));
+        var soucin = a.krat(b);
+        IO.println("Soucin je %s".formatted(soucin.zkratit()));
+        var podil = a.deleno(b);
+        IO.println("Podil je %s".formatted(podil.zkratit()));
+
+        Zlomek[] pole = new Zlomek[]{
+                new Zlomek(1, 2),
+                new Zlomek(3, 4),
+                new Zlomek(6, 5),
+                new Zlomek(4, 3),
+                new Zlomek(7, 8),
+                new Zlomek(13, 10),
+        };
+        Zlomek prumer = prumer(pole);
+        IO.println("Prumer je %s".formatted(prumer.zkratit()));
+    }
+
+    private static Zlomek prumer(Zlomek[] pole) {
+        var soucet = new Zlomek(0,1);
+        for (var z : pole) {
+            soucet = soucet.plus(z);
+        }
+        var pocet = new Zlomek(pole.length, 1);
+        return soucet.deleno(pocet);
     }
 }
--- src/main/java/cz/uhk/zlomky/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -4,11 +4,12 @@ public class Zlomek {
     private int citatel;
     private int jmenovatel;
 
-    public Zlomek() {}
+    public Zlomek() {
+    }
 
     public Zlomek(int citatel, int jmenovatel) {
-    this.citatel = citatel;
-    this.jmenovatel = jmenovatel;
+        this.citatel = citatel;
+        this.jmenovatel = jmenovatel;
     }
 
 
@@ -16,20 +17,86 @@ public class Zlomek {
     public int getCitatel() {
         return citatel;
     }
+
     public int getJmenovatel() {
         return jmenovatel;
     }
 
     //"Setter"
-    public void setCitatel(int citatel){
+    public void setCitatel(int citatel) {
         this.citatel = citatel;
     }
-    public void setJmenovatel(int jmenovatel){
+
+    public void setJmenovatel(int jmenovatel) {
         this.jmenovatel = jmenovatel;
     }
 
     @Override
     public String toString() {
-        return String.format("%4d\n-------\n%4d",citatel,jmenovatel);
+        return String.format("%d/%d", citatel, jmenovatel);
     }
+
+
+    public Zlomek zkratit() {
+        int delitel = nsd(citatel, jmenovatel); //vypocet nejmensiho spolecneho delitele
+        return new Zlomek(citatel / delitel, jmenovatel / delitel);
+    }
+
+    /**
+     * vypocet nejmensiho spolecneho delitele cisel A a B
+     *
+     * @param a citatel
+     * @param b jmenovatel
+     * @return
+     */
+    private int nsd(int a, int b) {
+        if (a < b) {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        int zbytek = 0;
+        do {
+            zbytek = a % b;
+            a = b;
+            b = zbytek;
+        } while (zbytek != 0);
+        return a;
+    }
+
+    /**
+     * Metoda pro scitani 2 zlomku
+     *
+     * @param b druhy zlomek
+     * @return vraci novy zlomek = soucet zlomku
+     */
+
+    public Zlomek plus(Zlomek b) {
+        int jm = jmenovatel * b.jmenovatel;
+        int cit = citatel * b.jmenovatel + b.citatel * jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek minus(Zlomek b) {
+        int jm = jmenovatel * b.jmenovatel;
+        int cit = citatel * b.jmenovatel - b.citatel * jmenovatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek krat(Zlomek b) {
+        int jm = jmenovatel * b.jmenovatel;
+        int cit = citatel * b.citatel;
+
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek deleno(Zlomek b) {
+        int jm = jmenovatel * b.citatel;
+        int cit = citatel * b.jmenovatel;
+
+        return new Zlomek(cit,jm);
+    }
+
 }
