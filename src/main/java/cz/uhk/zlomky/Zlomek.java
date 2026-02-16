ae787723adabc1117f7a881f88aa9b85a2361292
sterbto1 <sterbto1@uhk.cz>
2026-02-16 11:22:16 +0100

    2. Cviceni -> 1. zmena (upraveny poznamky)

--- src/main/java/cz/uhk/zlomky/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -12,7 +12,7 @@ public class Zlomek {
     }
 
 
-    //getter
+    //"Getter"
     public int getCitatel() {
         return citatel;
     }
@@ -20,7 +20,7 @@ public class Zlomek {
         return jmenovatel;
     }
 
-    //setter
+    //"Setter"
     public void setCitatel(int citatel){
         this.citatel = citatel;
     }
